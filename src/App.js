f5fab388b255c7514a6b5dce4b72f7be25b72856
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-12-08 10:45:04 -0300

    fix JSX problems and created the Input Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Input from './components/Input.js'
 
 class App extends Component {
   state = {
@@ -6,11 +7,13 @@ class App extends Component {
     idade: 0,
     cpf: ''
   }
-  
-  onSubmit = () => {
-    let { idade, nome, cpf } = this.state
+
+  onSubmit = (event) => {
+    event.preventDefault()
     
-    fetch( API_URL, {
+    let { idade, nome, cpf } = this.state
+
+    fetch( 'API_URL', {
       headers: {
         "content-type": "application/json"
       },
@@ -26,21 +29,36 @@ class App extends Component {
     .then(response => console.log(response))
     .catch(erro => console.log(erro))
   }
-  
+
   render() {
     return (
       <div>
-        <form onSubmit={(e) => { 
-          e.preventDefault
-          this.onSubmit()
-        }>
-          <input type="text" placeholder="Digite o nome" name="nome" value={ this.state.nome } onChange={ (e) => this.setState({ nome: this.target.value }) }/>
-          <input type="text" placeholder="Digite o cpf" name="cpf" value={ this.state.cpf } onChange={ (e) => this.setState({ cpf: this.target.value }) }/>
-          <input type="number" placeholder="Digite a idade" name="idade" value={ this.state.idade }onChange={ (e) => this.setState({ idade: this.target.value }) } />
+        <form onSubmit={this.onSubmit}>
+          <Input
+            type='text'
+            placeholder='Digite o nome'
+            name='nome'
+            value={this.state.nome}
+            onChange={(e) => this.setState({ nome: e.target.value })}/>
+
+          <Input
+          type="text"
+          placeholder="Digite o cpf"
+          name="cpf"
+          value={this.state.cpf}
+          onChange={ (e) => this.setState({ cpf: e.target.value }) }/>
+
+          <Input
+          type="text"
+          placeholder="Digite a idade"
+          name="idade"
+          value={this.state.idade}
+          onChange={ (e) => this.setState({ idade: e.target.value }) }/>
+
           <button type="submit">Cadastrar</button>
         </form>
       </div>
-    );
+    )
   }
 }
 
