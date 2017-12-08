3b53fa9d22670ea7e15046c2e66d31669e65384f
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-12-08 09:46:21 -0300

    uma 'ajudinha'
    dei uma agilizada pra você, dê uma lida nos links que eu te mandei e tente entender o que está acontecendo aqui, tente mudar algo, veja se funciona.
    
    O .then() é chamado se o AJAX der certo, você pode usar o retorno para atualiza o estado de algum outro componente, por exemplo.
    O .catch() é chamado se o AJAX der errado, você pode usar para avisar o usuário de algum erro que ele pode ter cometido, ou algum problema no servidor!
    
    vlw

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    nome: '',
+    idade: 0,
+    cpf: ''
+  }
+  
+  onSubmit = () => {
+    let { idade, nome, cpf } = this.state
+    
+    fetch( API_URL, {
+      headers: {
+        "content-type": "application/json"
+      },
+      method: 'POST',
+      body: JSON.stringify(
+        {
+          nome: nome,
+          idade: idade,
+          cpf: cpf
+        }
+      )
+    })
+    .then(response => console.log(response))
+    .catch(erro => console.log(erro))
+  }
+  
   render() {
     return (
-      <div className="App">
-        <input type="text" placeholder="Digite o nome" name="nome"/>
-        <input type="text" placeholder="Digite o cpf" name="cpf"/>
-        <input type="number" placeholder="Digite a idade" name="idade"/>
-        <button type="submit">Cadastrar</button>
+      <div>
+        <form onSubmit={(e) => { 
+          e.preventDefault
+          this.onSubmit()
+        }>
+          <input type="text" placeholder="Digite o nome" name="nome" value={ this.state.nome } onChange={ (e) => this.setState({ nome: this.target.value }) }/>
+          <input type="text" placeholder="Digite o cpf" name="cpf" value={ this.state.cpf } onChange={ (e) => this.setState({ cpf: this.target.value }) }/>
+          <input type="number" placeholder="Digite a idade" name="idade" value={ this.state.idade }onChange={ (e) => this.setState({ idade: this.target.value }) } />
+          <button type="submit">Cadastrar</button>
+        </form>
       </div>
     );
   }
